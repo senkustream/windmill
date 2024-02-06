0543e524002754382be93ff1b97b38710b4fec5b
Kresna Satya <kresnasatya72@gmail.com>
2024-02-07 06:21:20 +0800

    update app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,10 +7,11 @@
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
     </style>
+    @vite('resources/css/app.css')
+    @stack('styles')
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="{{ asset('init-alpine.js') }}"></script>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <div class="flex h-screen bg-gray-50 dark:bg-gray-900">
@@ -25,5 +26,6 @@
         </div>
     </div>
     @yield('before-body')
+    @stack('scripts')
 </body>
 </html>
