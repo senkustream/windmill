c698315e131319cec18da1a55dcf3b9851bfa751
Kresna Satya <kresnasatya72@gmail.com>
2024-02-06 15:38:57 +0800

    fix css class in search input

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -230,7 +230,7 @@
                 <!-- Search input -->
                 <div class="flex justify-center flex-1 lg:mr-32">
                     <div class="relative w-full max-w-xl mr-6 focus-within:text-purple-500">
-                        <div class="absolute inset-y-0 flex-items-center pl-2">
+                        <div class="absolute inset-y-0 flex items-center pl-2">
                             <svg class="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                                 <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
                             </svg>
