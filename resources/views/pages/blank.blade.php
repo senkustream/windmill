a482b2d73673890f3d1391b767a7921c45c4af99
Kresna Satya <kresnasatya72@gmail.com>
2024-02-08 12:47:57 +0800

    create pages page

---
+++ resources/views/pages/blank.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+<h1 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">Blank</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ChartsController;
 use App\Http\Controllers\FormsController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ModalsController;
+use App\Http\Controllers\PagesController;
 use App\Http\Controllers\TablesController;
 use Illuminate\Support\Facades\Route;
 
@@ -27,3 +28,9 @@ Route::get('/charts', [ChartsController::class, 'index']);
 Route::get('/buttons', [ButtonsController::class, 'index']);
 Route::get('/modals', [ModalsController::class, 'index']);
 Route::get('/tables', [TablesController::class, 'index']);
+
+Route::get('/pages/login', [PagesController::class, 'login']);
+Route::get('/pages/register', [PagesController::class, 'register']);
+Route::get('/pages/forgot-password', [PagesController::class, 'forgotPassword']);
+Route::get('/pages/404', [PagesController::class, 'notFound']);
+Route::get('/pages/blank', [PagesController::class, 'blank']);
