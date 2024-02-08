f02743a2477e372266fc5c769acc479d8ce104f0
Kresna Satya <kresnasatya72@gmail.com>
2024-02-08 12:07:16 +0800

    create charts page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CardsController;
+use App\Http\Controllers\ChartsController;
 use App\Http\Controllers\FormsController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
@@ -19,3 +20,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/forms', [FormsController::class, 'index']);
 Route::get('/cards', [CardsController::class, 'index']);
+Route::get('/charts', [ChartsController::class, 'index']);
